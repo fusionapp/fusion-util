17e71b477032c460b0dfd9e70b27f28105629a41
Tristan Seligmann <mithrandi@mithrandi.net>
2015-09-18 17:13:02 +0200

    Fix __init__.py.

--- fusion_util/__init__.py
+++ fusion_util/__init__.py
@@ -1,7 +1,5 @@
-from ._version import __version__
-
-__all__ = ['__version__']
-
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+__all__ = ['__version__']
